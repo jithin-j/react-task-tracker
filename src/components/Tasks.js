a4edb8a8d5e83dc2668c90e729b5168fdd0d992a
jithin-j <jithinjagadeesh1@gmail.com>
2021-09-26 21:21:36 +0530

    Added Delete feauture

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,8 +1,9 @@
-const Tasks = ({ tasks }) => {
+import Task from "./Task";
+const Tasks = ({ tasks, onDelete }) => {
   return (
     <>
       {tasks.map((task) => (
-        <h3 key={task.id}>{task.text}</h3>
+        <Task key={task.id} task={task} onDelete={onDelete} />
       ))}
     </>
   );
